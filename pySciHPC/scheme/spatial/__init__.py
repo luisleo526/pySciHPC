4b1d71d9cb9987647994b9ab49c5cb3efd01c5cc
leo <luisleo526@yahoo.com>
2022-09-15 15:08:57 +0800

    debug

--- pySciHPC/scheme/spatial/__init__.py
+++ pySciHPC/scheme/spatial/__init__.py
@@ -1,5 +1,3 @@
 from .CCD import CCD
 from .UCCD import UCCD
 from .ENO import WENO_p, WENO_m, WENO_JS, WENO_Z, CRWENO, CRWENO_LD
-from .CuWENO_JS import cuda_WENO_JS, cuda_WENO_Z
-from .CuUCCD import cuda_UCCD
